c164003ba4a726179c675ec1481685809815b53f
DengZC2000 <zhichaodeng2023@163.com>
2024-05-08 18:44:17 +0800

    add hash pwd

--- fim_auth/auth_api/internal/handler/loginhandler.go
+++ fim_auth/auth_api/internal/handler/loginhandler.go
@@ -2,12 +2,11 @@ package handler
 
 import (
 	"FIM/common/response"
-	"net/http"
-
 	"FIM/fim_auth/auth_api/internal/logic"
 	"FIM/fim_auth/auth_api/internal/svc"
 	"FIM/fim_auth/auth_api/internal/types"
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"net/http"
 )
 
 func loginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
@@ -22,10 +21,11 @@ func loginHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 		l := logic.NewLoginLogic(r.Context(), svcCtx)
 		resp, err := l.Login(&req)
 		//if err != nil {
-		//	httpx.ErrorCtx(r.Context(), w, err)
+		//httpx.ErrorCtx(r.Context(), w, err)
 		//} else {
-		//	httpx.OkJsonCtx(r.Context(), w, resp)
+		//httpx.OkJsonCtx(r.Context(), w, resp)
 		//}
+
 		response.Response(r, w, resp, err)
 	}
 }
