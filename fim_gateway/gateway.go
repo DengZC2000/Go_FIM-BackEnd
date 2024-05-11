7b8c9e63aadfe61c26859fe6f3fde2f8ee0fdddb
DengZC2000 <zhichaodeng2023@163.com>
2024-05-11 10:35:50 +0800

    adjust gateway code

--- fim_gateway/gateway.go
+++ fim_gateway/gateway.go
@@ -14,40 +14,29 @@ import (
 	"strings"
 )
 
-//var serviceMap = map[string]string{
-//	"auth": "http://127.0.0.1:20021",
-//	"user": "http://127.0.0.1:20022",
-//}
+//	var serviceMap = map[string]string{
+//		"auth": "http://127.0.0.1:20021",
+//		"user": "http://127.0.0.1:20022",
+//	}
+type BaseResponse struct {
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data any    `json:"data"`
+}
 
-func gateway(res http.ResponseWriter, req *http.Request) {
-	// 匹配请求前缀 /api/auth/xx
-	regex, _ := regexp.Compile(`/api/(.*?)/`)
-	addrList := regex.FindStringSubmatch(req.URL.Path)
-	if len(addrList) != 2 {
-		res.Write([]byte("err"))
-		return
-	}
-
-	service := addrList[1]
-	addr := etcd.GetServiceAddr(config.Etcd, service+"_api")
-	if addr == "" {
-		fmt.Printf("不匹配的服务%s", service)
-		res.Write([]byte("err"))
-		return
-	}
-	remoteAddr := strings.Split(req.RemoteAddr, ":")
-	fmt.Println(remoteAddr)
-
-	// 请求认证服务地址
-	authAddr := etcd.GetServiceAddr(config.Etcd, "auth_api")
-	authUrl := fmt.Sprintf("http://%s/api/auth/authentication", authAddr)
-	authRequest, _ := http.NewRequest("POST", authUrl, nil)
+func FileResponse(msg string, res http.ResponseWriter) {
+	response := BaseResponse{Code: 10086, Msg: msg}
+	byteData, _ := json.Marshal(response)
+	res.Write(byteData)
+}
+func auth(authAddr string, res http.ResponseWriter, req *http.Request) bool {
+	authRequest, _ := http.NewRequest("POST", authAddr, nil)
 	authRequest.Header = req.Header
 	authRequest.Header.Set("valid_path", req.URL.Path)
 	authRes, err := http.DefaultClient.Do(authRequest)
 	if err != nil {
-		res.Write([]byte("认证服务错误"))
-		return
+		FileResponse("认证服务错误", res)
+		return false
 	}
 
 	type Response struct {
@@ -59,29 +48,60 @@ func gateway(res http.ResponseWriter, req *http.Request) {
 	authErr := json.Unmarshal(byteData, &authResponse)
 	if authErr != nil {
 		logx.Error(authErr)
-		res.Write([]byte("认证服务错误"))
-		return
+		FileResponse("认证服务错误", res)
+		return false
 	}
 	//认证不通过
 	if authResponse.Code != 0 {
 		res.Write(byteData)
-		return
+		return false
 	}
-
-	url := fmt.Sprintf("http://%s%s", addr, req.URL.String())
-	fmt.Println(url)
-	byteData, _ = io.ReadAll(req.Body)
-	proxyReq, _ := http.NewRequest(req.Method, url, bytes.NewBuffer(byteData))
+	return true
+}
+func proxy(proxyAddr string, res http.ResponseWriter, req *http.Request) {
+	byteData, _ := io.ReadAll(req.Body)
+	proxyReq, _ := http.NewRequest(req.Method, proxyAddr, bytes.NewBuffer(byteData))
 
 	proxyReq.Header = req.Header
 	proxyReq.Header.Del("valid_path")
 	response, ProxyErr := http.DefaultClient.Do(proxyReq)
 	if ProxyErr != nil {
-		fmt.Println(ProxyErr.Error())
-		res.Write([]byte("服务异常"))
+		logx.Error(ProxyErr.Error())
+		FileResponse("服务异常", res)
 		return
 	}
 	io.Copy(res, response.Body)
+}
+func gateway(res http.ResponseWriter, req *http.Request) {
+	// 匹配请求前缀 /api/auth/xx
+	regex, _ := regexp.Compile(`/api/(.*?)/`)
+	addrList := regex.FindStringSubmatch(req.URL.Path)
+	if len(addrList) != 2 {
+		FileResponse("请求前缀错误", res)
+		return
+	}
+
+	service := addrList[1]
+	addr := etcd.GetServiceAddr(config.Etcd, service+"_api")
+	if addr == "" {
+		logx.Errorf("不匹配的服务 %s", service)
+		FileResponse("不匹配的服务", res)
+		return
+	}
+	remoteAddr := strings.Split(req.RemoteAddr, ":")
+
+	// 请求认证服务地址
+	authAddr := etcd.GetServiceAddr(config.Etcd, "auth_api")
+	authUrl := fmt.Sprintf("http://%s/api/auth/authentication", authAddr)
+	proxyUrl := fmt.Sprintf("http://%s%s", addr, req.URL.String())
+	//打印日志
+	logx.Infof("%s %s", remoteAddr[0], proxyUrl)
+
+	if !auth(authUrl, res, req) {
+		return
+	}
+
+	proxy(proxyUrl, res, req)
 
 	//fmt.Println(req.URL.Path)
 }
@@ -91,6 +111,7 @@ var configFile = flag.String("f", "settings.yaml", "the config file")
 type Config struct {
 	Addr string
 	Etcd string
+	Log  logx.LogConf
 }
 
 var config Config
@@ -99,7 +120,7 @@ func main() {
 	flag.Parse()
 
 	conf.MustLoad(*configFile, &config)
-
+	logx.SetUp(config.Log)
 	//回调函数
 	http.HandleFunc("/", gateway)
 
