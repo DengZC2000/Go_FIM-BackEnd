ffeea4876fcb6801a0640a99dd46326e23c5be1a
DengZC2000 <zhichaodeng2023@163.com>
2024-06-13 22:18:59 +0800

    backstage auth and operate user_service

--- fim_user/user_api/internal/middleware/adminmiddleware.go
+++ fim_user/user_api/internal/middleware/adminmiddleware.go
@@ -16,7 +16,7 @@ func NewAdminMiddleware() *AdminMiddleware {
 func (m *AdminMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Header.Get("User-Role") != "1" {
-			response.Response(r, w, nil, errors.New("用户操作鉴权失败"))
+			response.Response(r, w, nil, errors.New("后台操作用户鉴权失败"))
 			return
 		}
 		next(w, r)
